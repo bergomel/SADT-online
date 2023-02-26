319f8fc73bfd9552d891ca796747c4d5ba6307a8
bergomel <bernardogomel@gmail.com>
2023-02-26 01:41:08 -0300

    path json

--- start.js
+++ start.js
@@ -1,7 +1,7 @@
 import tuss from '../procedimentos.json' assert {type: 'json'}
-import estados from '/estados-brasileiros.json' assert {type: 'json'}
-import tabela24 from '../tabela24.json' assert {type: 'json'}
-import tabela26 from '../tabela26.json' assert {type: 'json'}
+import estados from 'estados-brasileiros.json' assert {type: 'json'}
+import tabela24 from './tabela24.json' assert {type: 'json'}
+import tabela26 from '/tabela26.json' assert {type: 'json'}
 import cid from '../cid10.json'assert {type: 'json'}
 
 export default tuss
